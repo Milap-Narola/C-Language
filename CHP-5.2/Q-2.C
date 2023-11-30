dc0790627681bdeddbe42d1a28b9a02378329cf2
Milap Narola <148769224+Milap5322@users.noreply.github.com>
2023-11-30 13:40:57 +0530

    28\11\2023

---
+++ CHP-5.2/Q-2.C
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter A value:");
+    scanf("%d",&a);
+    printf("Enter B value:");
+    scanf("%d",&b);
+    printf("Enter C value:");
+    scanf("%d",&c);
+    printf("Enter D value:");
+    scanf("%d",&d);
+    
+     if(a>b)
+     {
+         if(a>c)
+         {
+           if(a>d)
+           {
+             printf("A is maximum");
+           }  
+         } 
+     }
+     if(b>a)
+     {
+         if(b>c)
+         {
+            if(b>d)
+            {
+                printf("B is maximum");
+            }
+         } 
+     }
+     if(c>a)
+     {
+         if(c>b)
+         {
+            if(c>d)
+            {
+                printf("c is maximum");
+            }
+         }
+     }
+     if(d>a)
+     {
+         if(d>b)
+         {
+            if(d>c)
+            {
+                printf("d is maximum");
+            }
+         }
+     }
+    return 0;
+}
