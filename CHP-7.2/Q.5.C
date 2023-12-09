fd9f07ed2c6c1bd20bd005d7bd3a2e6e2f26509b
Milap Narola <milap5322@gmail.com>
2023-12-09 17:26:27 +0530

    8/12/2023

---
+++ CHP-7.2/Q.5.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define p printf
+int main()
+{
+    int i, j, s;
+    for (i = 5; i >= 1; i--)
+    {
+        for (s = 5; s > i; s--)
+        {
+            p(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            p("%d", j%2==1);
+
+        }
+
+        p("\n");
+    }
+
+    return 0;
+}
