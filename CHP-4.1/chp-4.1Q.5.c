1b71b58636017b07f2b8cf083e98a8611575f66b
Milap Narola <milap5322@gmail.com>
2024-01-23 21:28:05 +0530

    ok

---
+++ CHP-4.1/chp-4.1Q.5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float a,b,c;
+    printf("enter any number");
+    scanf("%f",&a);
+    printf("enter any number");
+    scanf("%f",&b);
+    c = (a * a) + (b * b) + (3 * a * b);
+    printf("Square = %.2f\n",c);
+    return 0;
+}
