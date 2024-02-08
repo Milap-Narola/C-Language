48425f7ada994b737e8c80b5242b143d5473261b
Milap Narola <milap5322@gmail.com>
2024-02-08 22:55:56 +0530

    Done

---
+++ CHP-7.1/Q_24.C
@@ -0,0 +1,19 @@
+/*
+1 0 1 0 1  
+0 1 0 1    
+1 0 1      
+0 1       
+1   */
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = i; j >= 1; j--)
+        {
+            printf(" %d", j % 2);
+        }
+        printf("\n");
+    }
+}
---
+++ CHP-7.1/Q_7.C
@@ -0,0 +1,19 @@
+/*Q.7 Write a Program to print the below pattern using nested for loop.
+A
+B A
+C B A
+D C B A
+E D C B A*/
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 65; i <= 69; i++)
+    {
+        for (j = i; j >= 65; j--)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+}
