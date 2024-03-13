54b2ee6d98b56a15558b421fd5ee645c77ffcb01
Milap Narola <milap5322@gmail.com>
2024-03-13 14:43:26 +0530

    Done

---
+++ CHP-10.1/Q_1.C
@@ -0,0 +1,21 @@
+/*Q.1 Write a Program to find the cube of a given number using UDF.
+For example,
+Input:
+Enter any number: 5
+Output:
+Cube is: 125
+*/
+#include <stdio.h>
+int cuBe(int num)
+{
+    return num * num * num;
+}
+int main()
+{
+    int n;
+    printf("Enter Any Number:");
+    scanf("%d", &n);
+    int result = cuBe(5);
+    printf("5 Cubed is : %d\n", result);
+    return 0;
+}
