9e3c906b5a5ea7bdda81634256fb6f060fd7b257
Milap Narola <148769224+Milap-Narola@users.noreply.github.com>
2024-03-28 16:22:27 +0530

    Update Q_1.C

--- CHP-10.1/Q_1.C
+++ CHP-10.1/Q_1.C
@@ -15,7 +15,7 @@ int main()
     int n;
     printf("Enter Any Number:");
     scanf("%d", &n);
-    int result = cuBe(5);
+    int result = cuBe(n);
     printf("5 Cubed is : %d\n", result);
     return 0;
 }
